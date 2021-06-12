bd70f73d8d4ea9db29050a1c5aa4df1cc47d57b2
Peter Atkinson <peteratkinson1994@gmail.com>
2021-06-12 21:52:27 +0100

    Add modal changes

--- football-crime-frontend/src/components/Modal.vue
+++ football-crime-frontend/src/components/Modal.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="center">
-    <vs-dialog v-on:close="updateSelectedStadium(null)" :width="width" v-model="data">
+    <vs-dialog
+      v-on:close="updateSelectedStadium(null)"
+      :width="width"
+      v-model="data"
+    >
       <template #header>
         <slot name="header"></slot>
       </template>
 
       <div class="body">
-        <slot name="body"></slot>
+        {{ data }}
+        <div class="stadium-details__basic-info">
+          <div class="stadium-details__team-crest">
+            <div
+              v-bind:style="{ backgroundImage: 'url(' + data.stadium.team.crestUrl + ')' }"
+              class="stadium-details__team-crest--image"
+            ></div>
+          </div>
+
+           <div class="stadium-details__stadium-info">
+          <h1 class="stadium-details__title">{{data.stadium.name}} - {{data.stadium.team.name}}</h1>
+          <span class="text">{{data.stadium.address.street}}, {{data.stadium.address.postcode}}, {{data.stadium.address.country}}</span>
+        </div>
+           
+        </div>
       </div>
     </vs-dialog>
   </div>
 </template>
 <script>
-import { mapActions } from 'vuex'
+
+import { mapActions } from "vuex";
 
 export default {
   props: {
     data: null
   },
   methods: {
-    ...mapActions(['updateSelectedStadium'])
+    ...mapActions(["updateSelectedStadium"]),
   },
+  mounted() {},
   computed: {
-
     width() {
       switch (this.$mq) {
         case "sm":
@@ -37,3 +56,64 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.body {
+  padding: 24px;
+  padding-top: 37px;
+}
+.stadium-details {
+  &__title {
+    font-size: 23px;
+    margin-top: 0;
+    margin-bottom: 6px;
+    max-width: 65%;
+  }
+ 
+  &__team-details {
+    margin-left: 16px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__basic-info {
+    background: rgba(20,20,23, 1);
+    padding: 16px;
+    border-radius: 5px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  &__team-info {
+    display: flex;
+    flex-direction: row;
+  }
+  &__stadium-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 12px;
+    width: 100%;
+  }
+  &__team-crest {
+    &--image {
+      height: 75px;
+      width: 75px;
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+  }
+}
+
+ .text {
+      font-size: 14px;
+      margin-bottom: 4px;
+    }
+
+    .italic {
+      font-style: italic;
+    }
+</style>
